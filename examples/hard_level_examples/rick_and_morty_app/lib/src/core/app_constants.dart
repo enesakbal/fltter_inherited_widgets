98f45e9f7051dc9ade5d12f0dde816b911e5bcc3
Enes Akbal <enesakbal00@gmail.com>
2024-02-13 16:44:18 +0300

    feat: app constants

---
+++ examples/hard_level_examples/rick_and_morty_app/lib/src/core/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const baseUrl = 'rickandmortyapi.com';
+
+  static const charactersPath = '/api/character';
+  static const locationsPath = '/api/location';
+  static const episodesPath = '/api/episode';
+}
